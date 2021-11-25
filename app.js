5f9f5131087f4019fc65140a768522837786ffbf
Manish Sahu <manissh7352@gmail.com>
2021-11-25 06:21:32 +0530

    added connection to mongoDB

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-
-
-const express = require('express');
-
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //middleware, express.json(), if absent, we will not get data in request body
-app.use(express.json())
-
+app.use(express.json());
 
 //routes
-app.get('/hello', (req, res) => {
-    res.send('task manager app')
-})
+app.get("/hello", (req, res) => {
+  res.send("task manager app");
+});
 
-app.use('/api/v1/tasks', tasks)
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
